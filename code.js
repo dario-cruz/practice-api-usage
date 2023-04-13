d9392729a8a9a1bae1fa636c86249f44f7423cc8
Dario Cruz <rio2012@gmail.com>
2023-04-13 13:16:21 -0400

    Add heading to page, placed fetch code into function that executes on page load.

--- code.js
+++ code.js
@@ -1,18 +1,16 @@
 // Define the image element located in the dom.
 const imgElem = document.getElementById('random-img')
-console.log(imgElem)
-// Function for console.log of the result produced from the fetch promise.
-function consoleResult(result) {
-    console.log(result.json())
+
+fetchGif()
+
+function fetchGif() {
+// Fetch random image from giphy and display it on the page.
+    fetch('https://api.giphy.com/v1/gifs/translate?api_key=aUyhSOuOVKcUPOfGiOTj56j0HjuJJ8SQ&s=dogs', {mode: 'cors'})
+        .then(function(response) {
+        return response.json()
+        })
+        .then(function(response) {
+        imgElem.src = response.data.images.original.url
+        })    
 }
 
-
-
-// Fetch random image from giphy and display it on the page.
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=aUyhSOuOVKcUPOfGiOTj56j0HjuJJ8SQ&s=cats', {mode: 'cors'})
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(response) {
-        imgElem.src = response.data.images.original.url
-    })    
