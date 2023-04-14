07ccd15d94f38f06de33adda397b5b0d70aa414a
Dario Cruz <rio2012@gmail.com>
2023-04-14 15:31:51 -0400

    Re-factor promise code to use async func and await.

--- code.js
+++ code.js
@@ -4,15 +4,19 @@ const newImageButton = document.getElementById('re-fresh')
 
 fetchGif()
 
-function fetchGif() {
-// Fetch random image from giphy and display it on the page.
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=aUyhSOuOVKcUPOfGiOTj56j0HjuJJ8SQ&s=dogs', {mode: 'cors'})
-        .then(function(response) {
-        return response.json()
-        })
-        .then(function(response) {
-        imgElem.src = response.data.images.original.url
-        })    
+async function fetchGif() {
+    // Fetch random image from giphy and display it on the page.
+    const imageRequest = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=aUyhSOuOVKcUPOfGiOTj56j0HjuJJ8SQ&s=dogs', {mode: 'cors'})
+    const imageData = await imageRequest.json()
+    imgElem.src = imageData.data.images.original.url
+
+//     fetch('https://api.giphy.com/v1/gifs/translate?api_key=aUyhSOuOVKcUPOfGiOTj56j0HjuJJ8SQ&s=dogs', {mode: 'cors'})
+//         .then(function(response) {
+//         return response.json()
+//         })
+//         .then(function(response) {
+//         imgElem.src = response.data.images.original.url
+//         })    
 }
 
 newImageButton.addEventListener('click', fetchGif)
